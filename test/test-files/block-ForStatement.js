0f45514271ff90e1c186d9ceb4583ad2bfef2d4b
Todd Wolfson <todd@twolfson.com>
2014-11-04 19:40:37 -0800

    Added for loop

---
+++ test/test-files/block-ForStatement.js
@@ -0,0 +1,7 @@
+// TODO: We should test when it is inside the braces
+for (let i = 1, block = true; i < 10; i++) {
+  // Code goes here
+}
+for (var lexical = 'hello'; false; false) {
+  // Code goes here
+}
