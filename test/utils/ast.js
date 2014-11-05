cf9d02e1534df292ff95adfd117e9e080e7451d9
Todd Wolfson <todd@twolfson.com>
2014-11-04 19:22:20 -0800

    Adding AST utility

---
+++ test/utils/ast.js
@@ -0,0 +1,25 @@
+// Load in dependencies
+var _ = require('underscore');
+var astw = require('astw');
+
+// Define AST utilities
+exports.findFirst = function (ast, requisites) {
+  // Walk the AST looking for a matching node
+  var retVal = null;
+  var comparator = _.matches(requisites);
+  astw(ast).walk(function compareNode (node) {
+    // If we have found our retVal, do nothing
+    if (retVal) {
+      return;
+    }
+
+    // If we find a node that matches our requisites, save it and
+    if (comparator(node)) {
+      retVal = node;
+      return;
+    }
+  });
+
+  // Return the result
+  return retVal;
+};
