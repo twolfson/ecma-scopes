ae833c45e1e6d557708539a82c3ffdbff955839a
Todd Wolfson <todd@twolfson.com>
2014-11-04 16:06:51 -0800

    Defining test content

--- test/ecma-scopes-test.js
+++ test/ecma-scopes-test.js
@@ -1,11 +1,32 @@
-var ecma_scopes = require('../');
+// Load in dependencies
+var fs = require('fs');
+var vm = require('vm');
 
-describe('ecma-scopes', function () {
-  before(function () {
+// Define test utilities
+var testUtils = {
+  openVm: function (filepath) {
+    before(function openVmFn () {
+      // Load our file into the VM
+      var script = fs.readFileSync(filepath, 'utf8');
+      this.vm = {};
+      vm.runInNewContext(script, this.vm);
+    });
+    after(function cleanup () {
+      // Clean up the vm
+      delete this.vm;
+    });
+  }
+};
 
-  });
+// Define our tests
+describe('ecma-scopes\' lexical scopes:', function () {
+  // TODO: Load from JSON, convert to dash-case, and load file
+  // TODO: Then iterate in a `forEach` loop
+  describe('a "' + 'Function' + '"', function () {
+    testUtils.openVm(__dirname + '/test-files/lexical-function.js');
 
-  it('', function () {
-
+    it('is a lexical scope', function () {
+      console.log(this.vm);
+    });
   });
 });
