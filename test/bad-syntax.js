e464217c6360f04b9b39e9be699b324e59985a92
Todd Wolfson <todd@twolfson.com>
2014-11-04 22:39:36 -0800

    Reenabled all tests

--- test/bad-syntax.js
+++ test/bad-syntax.js
@@ -10,11 +10,9 @@ var scriptUtils = require('./utils/script');
 // DEV: For sanity we will parse the scripts with bad syntax
 // DEV: This will help us detect if `esprima` starts supporting something unexpected
 var dir = __dirname + '/test-files/';
-// TODO: Re-enable all flavors
-var files = glob.sync('bad-syntax/*-Swi*', {cwd: dir});
-// var files = glob.sync('bad-syntax/*', {cwd: dir});
+var files = glob.sync('bad-syntax/*', {cwd: dir});
 files.forEach(function handleBadSyntaxFile (_filepath) {
-  describe.only('Bad syntax file "' + _filepath + '"', function () {
+  describe('Bad syntax file "' + _filepath + '"', function () {
     // Construct full filepath
     // DEV: We use a `cwd` to make describe names easier to read
     var filepath = dir + _filepath;
