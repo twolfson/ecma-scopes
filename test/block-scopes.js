7222d8f85fb4a7e317446f1a010bc41376a78412
Todd Wolfson <todd@twolfson.com>
2014-11-04 19:35:20 -0800

    Starting on block-scopes tests

---
+++ test/block-scopes.js
@@ -0,0 +1,62 @@
+// Load in dependencies
+var expect = require('chai').expect;
+var ecmaScopes = require('../');
+var astUtils = require('./utils/ast');
+var scriptUtils = require('./utils/script');
+
+// Define our tests
+describe('ecma-scopes\' block scopes:', function () {
+  // TODO: Remove `slice`
+  ecmaScopes.block.slice(-1).forEach(function testBlockCase (type, typeIndex) {
+  // ecmaScopes.block.forEach(function testBlockCase (type, typeIndex) {
+    describe('a/an "' + type + '"', function () {
+      // Resolve and load our scope file
+      // e.g. `test-files/block-IfStatement.js`
+      var filepath = __dirname + '/test-files/block-' + type + '.js';
+      scriptUtils.load(filepath, type);
+
+      // Collect the parents for analysis within the lexical scope
+      before(function collectParents () {
+        // Find closest identifier to `root`
+        var node = this.node = astUtils.findFirst(this.ast, {
+          type: 'Identifier',
+          name: 'block'
+        });
+        expect(node).to.not.equal(null);
+
+        // Resolve the node's parents until we hit our scope type (e.g. `IfStatement`)
+        this.parents = astUtils.findParentsUntil(node, {
+          type: type
+        });
+      });
+
+      it('is a block scope', function () {
+        expect(this.vm).to.not.have.ownProperty('block');
+      });
+
+      it('is not a lexical scope', function () {
+        expect(this.vm).to.have.ownProperty('lexical');
+      });
+
+      it('contains `block` inside of a "' + type + '"', function () {
+        var container = this.parents[this.parents.length - 1];
+        expect(this.parents).to.not.have.length(0);
+        expect(container.type).to.equal(type);
+      });
+
+      it('does not contain `block` inside of lexical scopes and other block scopes', function () {
+        // Collect the lexical and other block scopes
+        var lexicalScopes = ecmaScopes.lexical;
+        var otherBlockScopes = ecmaScopes.block.slice();
+        otherBlockScopes.splice(typeIndex, 1);
+
+        // Verify each of the nodes is not in there
+        this.parents.forEach(function assertNotOtherScopes (parent) {
+          expect(parent.type).to.not.equal(undefined);
+          expect(lexicalScopes).to.not.contain(parent.type);
+          expect(otherBlockScopes).to.not.contain(parent.type);
+        });
+      });
+    });
+  });
+});
