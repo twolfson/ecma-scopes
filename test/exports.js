0c666613d9dd6031e09a9598b036a78292ae6af2
Todd Wolfson <todd@twolfson.com>
2014-11-04 22:05:26 -0800

    Added test to verify JSON is always up to date

---
+++ test/exports.js
@@ -0,0 +1,12 @@
+// Load our dependencies
+var expect = require('chai').expect;
+var ecmaScopesCommented = require('../lib/ecma-scopes.comments.js');
+var ecmaScopes = require('../');
+
+// Start our tests
+describe('Our commented set of scopes and our stringified set of scopes', function () {
+  it('do not have any discrepencies', function () {
+    // DEV: If this test fails, run `npm run stringify` to generate the latest set
+    expect(ecmaScopes).to.deep.equal(ecmaScopesCommented);
+  });
+});
