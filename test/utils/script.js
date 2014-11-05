d9d429b6738ef9e37b428bd30c348280b3a0fb3d
Todd Wolfson <todd@twolfson.com>
2014-11-04 22:18:38 -0800

    Added asserts against unrunnable scopes being unrunnable

--- test/utils/script.js
+++ test/utils/script.js
@@ -1,4 +1,5 @@
 // Load our dependencies
+var assert = require('assert');
 var fs = require('fs');
 var vm = require('vm');
 var astw = require('astw');
@@ -28,11 +29,26 @@ exports.loadScript = function (filepath) {
   });
 };
 
-exports.runVm = function () {
+exports.runVm = function (type) {
   before(function openVmFn () {
-    // Load our file into the VM
+    // Attempt to load our file into the VM
     this.vm = {};
-    vm.runInNewContext(this.script, this.vm);
+    var err;
+    try {
+      vm.runInNewContext(this.script, this.vm);
+    // If an error occurs, save it
+    } catch (_err) {
+      err = _err;
+    }
+
+    // If we were an unrunnable scope, verify there was an error
+    if (exports.unrunnableScopes.indexOf(type) !== -1) {
+      assert(err, 'Expected `err` to be defined but was `' +  err + '` for "' + type + '"');
+      delete this.vm;
+    // Otherwise, if there was an error, raise it
+    } else if (err) {
+      throw err;
+    }
   });
   after(function cleanup () {
     // Clean up the vm
@@ -42,7 +58,5 @@ exports.runVm = function () {
 
 exports.load = function (filepath, type) {
   exports.loadScript(filepath);
-  if (exports.unrunnableScopes.indexOf(type) === -1) {
-    exports.runVm();
-  }
+  exports.runVm(type);
 };
